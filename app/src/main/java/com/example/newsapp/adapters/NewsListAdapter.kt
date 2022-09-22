05164bd331ed7802ff37f9991b84ca172d0bf11f
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2022-09-22 02:42:19 +0200

    setup view adapter

---
+++ app/src/main/java/com/example/newsapp/adapters/NewsListAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.newsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.databinding.ArticleViewBinding
+import com.example.newsapp.model.Article
+
+class NewsListAdapter :RecyclerView.Adapter<NewsListAdapter.ArticleViewHolder>(){
+
+    inner class ArticleViewHolder(val binding: ArticleViewBinding): RecyclerView.ViewHolder(binding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val binding = ArticleViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ArticleViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        with(holder){
+            Glide.with(binding.root).load(article.urlToImage).into(binding.ivArticleImage)
+            binding.tvSource.text = article.source.name
+            binding.tvTitle.text = article.title
+            binding.tvDescription.text = article.description
+            binding.tvPublishedAt.text = article.publishedAt
+            binding.articleView.setOnClickListener {
+                onItemClickListener?.let { it(article) }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+}
--- app/src/main/java/com/example/newsapp/model/Article.kt
+++ app/src/main/java/com/example/newsapp/model/Article.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
